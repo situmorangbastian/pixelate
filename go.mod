eca793bf0772e4680c0a02829094be43da488d71
Bastian Situmorang <bastianpaskal@gmail.com>
2024-03-13 18:13:04 +0700

    feat: add image service interface and add unit test for image service (#5)

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.22.1
 require (
 	github.com/gofiber/fiber/v2 v2.52.2
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -19,6 +21,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
---
+++ service/image_test.go
@@ -0,0 +1,187 @@
+package service_test
+
+import (
+	"bytes"
+	"image"
+	"image/color"
+	"image/png"
+	"mime/multipart"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/situmorangbastian/pixelate/service"
+)
+
+func TestConvertPngToJpg(t *testing.T) {
+	tests := []struct {
+		testName        string
+		invalidFileName string
+		expectedResult  string
+		expectedError   bool
+	}{
+		{
+			testName:       "success",
+			expectedResult: "converted.jpg",
+		},
+		{
+			testName:        "error on open file",
+			invalidFileName: "invalid.png",
+			expectedError:   true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			pngContent := createPNGFile()
+			pngFile, err := os.CreateTemp("", "test-*.png")
+			require.NoError(t, err)
+			defer os.Remove(pngFile.Name())
+			defer pngFile.Close()
+			_, err = pngFile.Write(pngContent)
+			require.NoError(t, err)
+
+			service := service.NewImageService()
+
+			fileHeader := &multipart.FileHeader{
+				Filename: pngFile.Name(),
+				Size:     int64(len(pngContent)),
+			}
+
+			if test.invalidFileName != "" {
+				fileHeader.Filename = test.invalidFileName
+			}
+
+			fileName, err := service.ConvertPngToJpg(fileHeader)
+			if test.expectedError {
+				require.Error(t, err)
+				return
+			}
+			defer os.Remove(test.expectedResult)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedResult, fileName)
+		})
+	}
+}
+
+func TestResize(t *testing.T) {
+	tests := []struct {
+		testName        string
+		invalidFileName string
+		expectedResult  string
+		expectedError   bool
+	}{
+		{
+			testName:       "success",
+			expectedResult: "resized.png",
+		},
+		{
+			testName:        "error on open file",
+			invalidFileName: "invalid.png",
+			expectedError:   true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			pngContent := createPNGFile()
+			pngFile, err := os.CreateTemp("", "test-*.png")
+			require.NoError(t, err)
+			defer os.Remove(pngFile.Name())
+			defer pngFile.Close()
+			_, err = pngFile.Write(pngContent)
+			require.NoError(t, err)
+
+			service := service.NewImageService()
+
+			fileHeader := &multipart.FileHeader{
+				Filename: pngFile.Name(),
+				Size:     int64(len(pngContent)),
+			}
+
+			if test.invalidFileName != "" {
+				fileHeader.Filename = test.invalidFileName
+			}
+
+			fileName, err := service.Resize(fileHeader, "10:10")
+			if test.expectedError {
+				require.Error(t, err)
+				return
+			}
+			defer os.Remove(test.expectedResult)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedResult, fileName)
+		})
+	}
+}
+
+func TestCompress(t *testing.T) {
+	tests := []struct {
+		testName        string
+		invalidFileName string
+		expectedResult  string
+		expectedError   bool
+	}{
+		{
+			testName:       "success",
+			expectedResult: "compressed.png",
+		},
+		{
+			testName:        "error on open file",
+			invalidFileName: "invalid.png",
+			expectedError:   true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			pngContent := createPNGFile()
+			pngFile, err := os.CreateTemp("", "test-*.png")
+			require.NoError(t, err)
+			defer os.Remove(pngFile.Name())
+			defer pngFile.Close()
+			_, err = pngFile.Write(pngContent)
+			require.NoError(t, err)
+
+			service := service.NewImageService()
+
+			fileHeader := &multipart.FileHeader{
+				Filename: pngFile.Name(),
+				Size:     int64(len(pngContent)),
+			}
+
+			if test.invalidFileName != "" {
+				fileHeader.Filename = test.invalidFileName
+			}
+
+			fileName, err := service.Compress(fileHeader)
+			if test.expectedError {
+				require.Error(t, err)
+				return
+			}
+			defer os.Remove(test.expectedResult)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedResult, fileName)
+		})
+	}
+}
+
+func createPNGFile() []byte {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	blue := color.RGBA{0, 0, 255, 255}
+
+	for x := 0; x < img.Bounds().Dx(); x++ {
+		for y := 0; y < img.Bounds().Dy(); y++ {
+			img.Set(x, y, blue)
+		}
+	}
+
+	var buf bytes.Buffer
+	err := png.Encode(&buf, img)
+	if err != nil {
+		panic(err)
+	}
+
+	return buf.Bytes()
+}
